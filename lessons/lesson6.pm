1411bba664fcd143d7dd9beecd3adb10090e4343
Joel Murphy <joelmurphy444@gmail.com>
2024-02-11 22:25:30 +0000

    Update lesson6.pm

--- lessons/lesson6.pm
+++ lessons/lesson6.pm
@@ -19,7 +19,7 @@ __DATA__
 <h2>Hello sigil</h2>
 
 One thing that you have to know about Perl is that variables are prefixed by special a character.<br/>
-You should see it as a good thing, because it allows variable interpolation into strings and helps the programmer know what kind of data they're dealing with...<br/><br/>
+You should see it as a good thing, because it allows variable interpolation into strings and helps the programmer to know what kind of data they're dealing with...<br/><br/>
 The most common/simple variable is the <strong>scalar</strong> value and it's prefixed by a <strong>stricken through S</strong>.<br/><br/>
 Perl users call it <strong>sigil</strong> but normal people call it <strong>dollar</strong>.<br/>
 Here is the famous symbol : <strong>$</strong><br/><br/>
