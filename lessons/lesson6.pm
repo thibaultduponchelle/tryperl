eeaa127c2ee19487522ed5d8b4115a119604dfeb
Joel Murphy <joelmurphy444@gmail.com>
2024-02-11 22:30:03 +0000

    Fix some typos

--- lessons/lesson6.pm
+++ lessons/lesson6.pm
@@ -18,13 +18,13 @@ __DATA__
 
 <h2>Hello sigil</h2>
 
-One thing that you have to know about Perl is that variables are prefixed by special a character.<br/>
+One thing that you have to know about Perl is that variables are prefixed by a special character.<br/>
 You should see it as a good thing, because it allows variable interpolation into strings and helps the programmer to know what kind of data they're dealing with...<br/><br/>
 The most common/simple variable is the <strong>scalar</strong> value and it's prefixed by a <strong>stricken through S</strong>.<br/><br/>
 Perl users call it <strong>sigil</strong> but normal people call it <strong>dollar</strong>.<br/>
 Here is the famous symbol : <strong>$</strong><br/><br/>
 
-As we don't even talked about what could contain a scalar variable, let's declare an undefined variable !<br/><br/>
+As we haven't even talked about what could contain a scalar variable, let's declare an undefined variable !<br/><br/>
 
 Declare your first variable like this : <code>$u = undef</code>
 
